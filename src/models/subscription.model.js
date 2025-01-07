d6ede712b05f4c8f264961fd0a2cdd10e594d5e1
umeraslam00 <joreypull@gmail.com>
2025-01-07 09:44:38 +0500

    add subscription model for user-channel relationships

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+
+const subscriptionSchema = new mongoose.Schema({
+    user: {
+        //This is the user who is subscribing
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+
+    channel: {
+        //One to whom the user is subscribing.
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+
+}, { timestamps: true })
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
